6f6d4f7632ef0b1fe5b0b5ef9711c209778dd39b
ElainePark2007 <eapark@nic.edu>
2024-10-24 23:25:23 +0000

    add function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
-
+int add(int a, int b);
 int main ()
 {
     cout << "Hello World!\n";
+    cout << "The summ of three and four is "<<add(3,4)<< endl;
     return 0;
+}
+
+int add (int a, int b)
+{
+    return (a+b);
 }
