e5eb937a2f6428ae9e98e33cb53f9d6a53a137fc
ElainePark2007 <eapark@nic.edu>
2024-10-24 16:29:37 -0700

    say hi

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 
+void say(string s);
 int main ()
 {
     cout << "Greeting\n";
     cout << "Hello World!\n";
+    say("hi");
     return 0;
+}
+
+void say(string s)
+{
+    cout << s << endl;
 }
